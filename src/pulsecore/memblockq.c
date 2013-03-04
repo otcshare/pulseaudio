5d50cd9f7d2a23ed179e8d4e5186031c4e21afe5
Jaska Uimonen <jaska.uimonen@intel.com>
2013-03-04 09:24:37 +0200

    fix draining not to lose samples in pre buffer

--- src/pulsecore/memblockq.c
+++ src/pulsecore/memblockq.c
@@ -797,6 +797,19 @@ void pa_memblockq_prebuf_disable(pa_memblockq *bq) {
     bq->in_prebuf = false;
 }
 
+void pa_memblockq_prebuf_disable_and_adjust(pa_memblockq *bq) {
+    size_t len;
+
+    pa_assert(bq);
+
+    bq->in_prebuf = false;
+
+    if (bq->prebuf > 0 && (len = pa_memblockq_get_length(bq)) > 0) {
+	pa_log("adjusting prebuf length to %u", len);
+	bq->prebuf = len;
+    }
+}
+
 void pa_memblockq_prebuf_force(pa_memblockq *bq) {
     pa_assert(bq);
 
