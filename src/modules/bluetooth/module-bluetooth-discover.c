1bffe72b4dc6fe35da2692f30494984d526d1d2c
vivian,zhang <vivian.zhang@intel.com>
2013-06-18 16:23:45 +0800

    add bluetooth a2dp aptx codec support - samsung
    Change-Id: I2c90198774c1e7d3e2ecb99f2dd365d56308f157
    Signed-off-by: Jaska Uimonen <jaska.uimonen@intel.com>

--- src/modules/bluetooth/module-bluetooth-discover.c
+++ src/modules/bluetooth/module-bluetooth-discover.c
@@ -34,6 +34,17 @@ PA_MODULE_DESCRIPTION("Detect available Bluetooth daemon and load the correspond
 PA_MODULE_VERSION(PACKAGE_VERSION);
 PA_MODULE_LOAD_ONCE(true);
 
+#ifdef BLUETOOTH_APTX_SUPPORT
+PA_MODULE_USAGE("aptx_lib_name=<name of aptx library name>");
+#endif
+
+#ifdef BLUETOOTH_APTX_SUPPORT
+static const char* const valid_modargs[] = {
+    "aptx_lib_name",
+    NULL
+};
+#endif
+
 struct userdata {
     uint32_t bluez5_module_idx;
     uint32_t bluez4_module_idx;
@@ -43,7 +54,29 @@ int pa__init(pa_module* m) {
     struct userdata *u;
     pa_module *mm;
 
+#ifdef BLUETOOTH_APTX_SUPPORT
+    pa_modargs *ma = NULL;
+    const char *aptx_lib_name = NULL;
+#endif
+
     pa_assert(m);
+
+#ifdef BLUETOOTH_APTX_SUPPORT
+    if (!(ma = pa_modargs_new(m->argument, valid_modargs))) {
+        pa_log("Failed to parse module arguments");
+        goto fail;
+    }
+
+    if (pa_modargs_get_value(ma, "async", NULL))
+        pa_log_warn("The 'async' argument is deprecated and does nothing.");
+
+
+    aptx_lib_name = pa_modargs_get_value(ma, "aptx_lib_name", NULL);
+    if (aptx_lib_name)
+        pa_load_aptx(aptx_lib_name);
+    else
+        pa_log("Failed to parse aptx_lib_name argument.");
+#endif
 
     m->userdata = u = pa_xnew0(struct userdata, 1);
     u->bluez5_module_idx = PA_INVALID_INDEX;
@@ -82,6 +115,10 @@ void pa__done(pa_module* m) {
 
     if (u->bluez4_module_idx != PA_INVALID_INDEX)
         pa_module_unload_by_index(m->core, u->bluez4_module_idx, true);
+
+#ifdef BLUETOOTH_APTX_SUPPORT
+    pa_unload_aptx();
+#endif
 
     pa_xfree(u);
 }
