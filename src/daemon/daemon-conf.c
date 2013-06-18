6da6989bbf0e2753c938197752c15ab33de5ccaa
vivian,zhang <vivian.zhang@intel.com>
2013-06-18 16:20:04 +0800

    add support for dlog - samsung

--- src/daemon/daemon-conf.c
+++ src/daemon/daemon-conf.c
@@ -188,9 +188,18 @@ int pa_daemon_conf_set_log_target(pa_daemon_conf *c, const char *string) {
 
         if (!log_target)
             return -1;
-    }
 
-    c->log_target = log_target;
+	c->log_target = log_target;
+    }
+#ifdef USE_DLOG
+    else if (!strcmp(string, "dlog")) {
+        c->auto_log_target = 0;
+        c->log_target = PA_LOG_DLOG;
+    } else if (!strcmp(string, "dlog-color")) {
+            c->auto_log_target = 0;
+            c->log_target = PA_LOG_DLOG_COLOR;
+    }
+#endif
 
     return 0;
 }
