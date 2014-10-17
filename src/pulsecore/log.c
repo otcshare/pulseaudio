1ac726d5c0c281ca05fb3ddadf044ca8f821a141
Colin Guthrie <colin@mageia.org>
2014-10-17 13:32:03 +0200

    build-sys: Clarify some systemd sub-component variable/define names.
    We currently use the term SYSTEMD when referring to libsystemd-login
    and JOURNAL when referring to libsystemd-journal.
    
    I will be shortly adding support for libsystemd-daemon and in
    preparation I figured it would be a good idea to clarify the names
    used currently before adding another!
    
    Change-Id: I1e513087eedce4371c2c0a992faa0281ecf0fa6a

--- src/pulsecore/log.c
+++ src/pulsecore/log.c
@@ -40,7 +40,7 @@
 #include <syslog.h>
 #endif
 
-#ifdef HAVE_JOURNAL
+#ifdef HAVE_SYSTEMD_JOURNAL
 #include <systemd/sd-journal.h>
 #endif
 
@@ -117,7 +117,7 @@ static const int level_to_syslog[] = {
 
 /* These are actually equivalent to the syslog ones
  * but we don't want to depend on syslog.h */
-#ifdef HAVE_JOURNAL
+#ifdef HAVE_SYSTEMD_JOURNAL
 static const int level_to_journal[] = {
     [PA_LOG_ERROR]  = 3,
     [PA_LOG_WARN]   = 4,
@@ -166,7 +166,7 @@ int pa_log_set_target(pa_log_target *t) {
     switch (t->type) {
         case PA_LOG_STDERR:
         case PA_LOG_SYSLOG:
-#ifdef HAVE_JOURNAL
+#ifdef HAVE_SYSTEMD_JOURNAL
         case PA_LOG_JOURNAL:
 #endif
         case PA_LOG_NULL:
@@ -509,7 +509,7 @@ void pa_log_levelv_meta(
                 break;
 #endif
 
-#ifdef HAVE_JOURNAL
+#ifdef HAVE_SYSTEMD_JOURNAL
             case PA_LOG_JOURNAL:
                 if (sd_journal_send("MESSAGE=%s", t,
                                 "PRIORITY=%i", level_to_journal[level],
@@ -708,7 +708,7 @@ pa_log_target *pa_log_parse_target(const char *string) {
         t = pa_log_target_new(PA_LOG_STDERR, NULL);
     else if (pa_streq(string, "syslog"))
         t = pa_log_target_new(PA_LOG_SYSLOG, NULL);
-#ifdef HAVE_JOURNAL
+#ifdef HAVE_SYSTEMD_JOURNAL
     else if (pa_streq(string, "journal"))
         t = pa_log_target_new(PA_LOG_JOURNAL, NULL);
 #endif
@@ -742,7 +742,7 @@ char *pa_log_target_to_string(const pa_log_target *t) {
         case PA_LOG_SYSLOG:
             string = pa_xstrdup("syslog");
             break;
-#ifdef HAVE_JOURNAL
+#ifdef HAVE_SYSTEMD_JOURNAL
         case PA_LOG_JOURNAL:
             string = pa_xstrdup("journal");
             break;
