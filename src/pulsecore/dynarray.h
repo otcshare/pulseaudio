8803e04c10aead5891bbcf64105909f038fab58a
Tanu Kaskinen <tanu.kaskinen@linux.intel.com>
2014-12-30 00:47:22 +0200

    dynarray: Add pa_dynarray_last()
    Change-Id: I9661a4a6ea29a9fc53cb83ba2b4b33e10c57a155
    Forwarded: http://thread.gmane.org/gmane.comp.audio.pulseaudio.general/22563/focus=22567

--- src/pulsecore/dynarray.h
+++ src/pulsecore/dynarray.h
@@ -52,6 +52,9 @@ void pa_dynarray_append(pa_dynarray *array, void *p);
 /* Returns NULL if i is out of bounds. */
 void *pa_dynarray_get(pa_dynarray *array, unsigned i);
 
+/* Returns NULL if the array is empty. */
+void *pa_dynarray_last(pa_dynarray *array);
+
 /* Returns -PA_ERR_NOENTITY if the array is empty, and zero otherwise. */
 int pa_dynarray_remove_last(pa_dynarray *array);
 
